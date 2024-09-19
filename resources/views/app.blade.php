6e1fe1bd6071df89d29d49b244ae9bf66418e0fa
Muzifer01 <brunnno.beltran@gmail.com>
2024-09-18 22:58:36 -0600

    Commit inicial

---
+++ resources/views/app.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @include('partials.head')
+    <body data-bs-theme="dark">
+        @include('partials.header')
+        {{-- {{ !! $content !!}} --}}
+    </body>
+</html>
